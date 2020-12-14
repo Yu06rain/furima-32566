5584d1aa305a7b7ee4b64ad788d830b83c6e6099
Yu06rain <yu06rain@gmail.com>
2020-12-14 20:59:43 +0900

    商品一覧機能の実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"abe"}
-    email                 {"aaa@gmail.com"}
-    password              {"00000a"}
-    password_confirmation {"00000a"}
-    last_name             {"ぜんかく"}
-    first_name            {"ぜんかく"}
-    last_name_kana        {"ゼンカクカナ"}
-    first_name_kana       {"ゼンカクカナ"}
-    birth_day             {"2020-01-01"}
+    nickname              { 'abe' }
+    email                 { 'aaa@gmail.com' }
+    password              { '00000a' }
+    password_confirmation { '00000a' }
+    last_name             { 'ぜんかく' }
+    first_name            { 'ぜんかく' }
+    last_name_kana        { 'ゼンカクカナ' }
+    first_name_kana       { 'ゼンカクカナ' }
+    birth_day             { '2020-01-01' }
   end
 end
